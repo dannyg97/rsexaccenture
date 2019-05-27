89336752382f4f8887449e7bc0c6d9ea3eb7dfb7
SkyeEllen <skye.ellen14@gmail.com>
2019-05-27 14:47:54 +1000

    file for python image processing script

---
+++ image_processing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Read in images
+
+# Find circles for the lights - Hough circle transformation
+
+# Change colourspace (RGB -> HSV)
+
+# Find colours for each circle
+
+# Send to splunk
+
