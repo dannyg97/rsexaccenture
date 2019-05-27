d4fe66156ba58e4b0c8eba4387a8f660949a05f4
Judy <judy.liu215@gmail.com>
2019-05-27 14:35:21 +1000

    add js

---
+++ dashboardscript.js
@@ -0,0 +1,9 @@
+function activeRed(light) {
+    document.getElementById(light).style.backgroundColor = "red";
+    document.getElementById(light).style.boxShadow = "0px 0px 70px red";
+}
+  
+function activeGreen(light) {
+    document.getElementById(light).style.backgroundColor = "rgb(51, 255, 51)";
+    document.getElementById(light).style.boxShadow = "0px 0px 70px rgb(51, 255, 51)";
+}
